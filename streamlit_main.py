a52a9e2fe5d3479e27313933788fbb892278f761
최재혁 <chlwogur2211@naver.com>
2026-02-12 07:14:36 +0000

    코드변경

--- streamlit_main.py
+++ streamlit_main.py
@@ -9,10 +9,11 @@ st.title(':sparkles: lotto 생성기 :sparkles:')
 
 def generate_lotto():
     lotto = [i + 1 for i in range(45)]
-    return lotto
+    random.shuffle(lotto)
+    return lotto[:6]
 
 button = st.button('로또 번호 생성기')
 
 if button:
     for i in range(5):
-        st.subheader(f'{i + 1}번째 로또 번호')
+        st.subheader(f'{i + 1}번째 로또 번호: :green[{generate_lotto()}]')
